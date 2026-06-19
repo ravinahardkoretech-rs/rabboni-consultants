c98be2eb697acd45844e64ee814f3646c8a87b22
ravinahardkoretech-rs <ravina.hardkore.tech@gmail.com>
2026-06-19 14:44:32 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        cormorant: ['Cormorant Garamond', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+}
